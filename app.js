8d1ce6ac40bc4e2f30f43bd929052a6805de539a
candacechou <candace01250125@gmail.com>
2023-06-25 14:58:19 +0800

    build: add static files and json for model

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// static 
+app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.redirect('/movies')
 })
