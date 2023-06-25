fbaa1df7023bd31cf2a530297d61147a08d514e7
candacechou <candace01250125@gmail.com>
2023-06-25 16:44:26 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
-
+const movies = require('./public/jsons/movies.json').results
+const BASE_IMG_URL = 'https://movie-list.alphacamp.io/posters/'
 
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
@@ -12,14 +13,15 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.redirect('/movies');
 })
 app.get('/movies', (req, res) => {
-  res.render('index')
+  res.render('index', { movies, BASE_IMG_URL })
 })
-app.get('/movie/:id', (req, res) => {
+app.get('/movies/:id', (req, res) => {
   const id = req.params.id
-  res.send(`read movie:${id}`)
+  const movie = movies.find((mv) => mv.id.toString() === id)
+  res.render('detail', { movie, BASE_IMG_URL })
 })
 app.listen(port, () => {
   console.log('express server running on <http://localhost>:${port}')
